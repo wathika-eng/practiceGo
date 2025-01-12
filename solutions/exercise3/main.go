dc11900a71664df1010760ee96d7668f5f114d8e
wathika-eng <wathika02@gmail.com>
2025-01-12 21:42:32 +0300

    Remove IDE configuration files and add implementation for filtering numbers less than five

--- solutions/exercise3/main.go
+++ solutions/exercise3/main.go
@@ -1,1 +1,23 @@
-package exercise3
+package main
+
+import (
+	"fmt"
+)
+
+const Five = 5
+
+func lessThanFive(numS []int) []int {
+	newNums := make([]int, 0, 5)
+	for _, v := range numS {
+		if v <= Five {
+			newNums = append(newNums, v)
+		}
+	}
+	return newNums
+}
+
+func main() {
+	numS := []int{100, 1, 5, 60, 4, 20, 3, -9}
+	digits := lessThanFive(numS)
+	fmt.Printf("Numbers less than 5 in the array: %v\n", digits)
+}
